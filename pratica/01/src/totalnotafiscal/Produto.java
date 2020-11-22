4e626cf5221c50a5cddcd188e2142f0e5133f678
Thyago-C <49129985+Thyago-C@users.noreply.github.com>
2020-11-22 14:42:44 -0300

    Add files via upload

---
+++ pratica/01/src/totalnotafiscal/Produto.java
@@ -0,0 +1,22 @@
+package totalnotafiscal;
+import java.util.Scanner;
+
+public class Produto {
+    
+    String descricao;
+    float preco;
+    float aliquota;
+    Scanner ler = new Scanner(System.in);
+    
+    public void getProduto(){
+        
+        System.out.print("Descrição: ");
+        descricao = ler.nextLine();
+        System.out.print("Preço: ");
+        preco = ler.nextFloat();
+        System.out.print("Aliquota: ");
+        aliquota = ler.nextFloat();
+        
+    }
+    
+}
